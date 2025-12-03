fefe93d698ff02f473519702b4c8c55027c02748
Valentin Trimaille <Montspy@gmail.com>
2025-12-03 13:29:20 +0100

    update: append to existing ics to keep past events update: remove geo field

--- wodztoics/__init__.py
+++ wodztoics/__init__.py
@@ -3,18 +3,16 @@ from dotenv import dotenv_values
 from wodz import WODZ
 
 from zoneinfo import ZoneInfo
-from datetime import datetime, timezone
+from datetime import datetime, timedelta
 from ical.calendar import Calendar
 from ical.event import Event
-from ical.types import Geo
+from ical.store import EventStore
 from ical.calendar_stream import IcsCalendarStream
-
-from geopy.geocoders import Nominatim
+from ical.exceptions import CalendarParseError
 
 def load_config() -> dict[str, ]:
     DEFAULTS = {
         "API_BOOKINGS_URL": "https://wodz.app/subscriber/booking/subscriberNextBookings",
-        # "PORT": 3080,
         "COOKIE": "",
         "OUTPUT_ICS": "./wodz.ics"
     }
@@ -39,39 +37,60 @@ def main():
     print("WODZtoICS started")
     config = load_config()
 
+    # Load existing ics
+    if os.path.exists(config['OUTPUT_ICS']):
+        print("Load existing calendar")
+        with open(config['OUTPUT_ICS'], "r") as ics_file:
+            try:
+                calendar = IcsCalendarStream.calendar_from_ics(ics_file.read())
+                print(f"Got calendar with {len(list(calendar.timeline))} bookings")
+            except CalendarParseError as err:
+                print(f"Failed to parse ics file '{config['OUTPUT_ICS']}': {err}")
+    else:
+        calendar = Calendar()
+    
+    # Delete events 10 minutes after now. They will be re-created by pulling from wodz.app API
+    store = EventStore(calendar)
+    localtz = datetime.now().astimezone().tzinfo
+    soon = datetime.now().astimezone() + timedelta(seconds=1)
+    print(f"Deleting future events to avoid dupes")
+    for event in calendar.timeline_tz(localtz).start_after(soon):
+        print(f"\t{event.summary} from {event.dtstart} to {event.dtend} (UID={event.uid})")
+        store.delete(event.uid)
+
+    print(f"{len(list(calendar.timeline))} bookings left in calendar")
+
+    # Get WODZ bookings
     wodz = WODZ(config['API_BOOKINGS_URL'], config['parsed_cookies'])
-    print("Fetching bookings")
+    print("Fetching bookings from wodz.app API")
     bookings = wodz.fetch_bookings()
-    print(f"Got {len(bookings['bookings'])} bookings")
-
-    geolocator = Nominatim(user_agent="WODZtoICS")
-
-    calendar = Calendar()
+    print(f"Got {len(bookings['bookings'])} bookings, creating events")
+    
+    def utc_to_local(utc_dt: datetime) -> datetime:
+        return utc_dt.replace(tzinfo=ZoneInfo("UTC")).astimezone(tz=None)
+    
     for booking in bookings['bookings']:
         if booking['session']['address'] is None:
             entity = booking['session']['business_entity']
             address = f"{entity['Address']} {entity['zipcode']} {entity['city']}"
         else:
             address = booking['session']['address']
-        location = geolocator.geocode(address, country_codes=['fr'])
 
         comment = f"{booking['session']['maxBookingSlots'] - booking['session']['availableBookingSpots']}/{booking['session']['maxBookingSlots']}"
-
-        def utc_to_local(utc_dt: datetime) -> datetime:
-            return utc_dt.replace(tzinfo=ZoneInfo("UTC")).astimezone(tz=None)
 
         event_params = {
             "summary": booking['session']['training_category']['name'],
             "dtstart": utc_to_local(datetime.strptime(booking['session']['start_date'], r"%Y-%m-%d %H:%M:%S")),
             "dtend": utc_to_local(datetime.strptime(booking['session']['end_date'], r"%Y-%m-%d %H:%M:%S")),
-            "geo": Geo(location.latitude, location.longitude),
             "comment": [comment],
             "location": address,
         }
 
         calendar.events.append(
-            Event(**event_params),
+            Event(**event_params)
         )
+    
+    print(f"Calendar now has {len(list(calendar.timeline))} bookings")
     
     print(f"Writing calendar file to {config['OUTPUT_ICS']}")
 
